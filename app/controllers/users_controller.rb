9aeecf5a22c0f96b3e5d11838776769e303cc9b2
Aditya Chowdhry <adityachowdhry09@gmail.com>
2015-10-13 21:37:46 +0530

    Account verification and password rest facilities added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only:[:edit, :update]
   before_action :admin_user,     only: :destroy
+  
   # GET /users
   # GET /users.json
   def index
@@ -32,14 +33,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-      if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-      else
-       render 'new'
-      end
-
+    if @user.save
+  @user.send_activation_email
+  flash[:info] = "Please check your email to activate your account."
+  redirect_to root_url
+else
+  render 'new'
+end
   end
 
   # PATCH/PUT /users/1
